7e0d26d0f347fa4e2a1f6af0bac64cf571336452
Shravya Hegde <shravyahegde104@gmail.com>
2026-08-18 17:55:49 +0530

    Update Expense class

--- Expense.java
+++ Expense.java
@@ -4,18 +4,18 @@ public class Expense {
     String category;
     String description;
     LocalDate date;
-    Expense(int amount,String category,String description){        //constructor (whenever someone creates an expense ,
-    //  they need to provide an amount ,category, and description)
+    Expense(int amount,String category,String description){        
+    
         this.amount=amount;
         this.category=category;
         this.description=description;
-        this.date=LocalDate.now();  // whenever an expense is created , the date is automatically set to the current date
+        this.date=LocalDate.now();  
     }
-    Expense(int amount,String category,String description,LocalDate date){        //constructor (whenever someone creates an expense ,
-        //  they need to provide an amount ,category, and description)
+    Expense(int amount,String category,String description,LocalDate date){       
+        
         this.amount=amount;
         this.category=category;
         this.description=description;
-        this.date=date;  // whenever an expense is created , the date is automatically set to the current date
+        this.date=date;  
     }
 }
